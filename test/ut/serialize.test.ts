cb7d415fa2b69076784d294e100973dbc246f98c
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-08-18 11:00:47 +0300

    Make sure that all important classes are serializable

--- test/ut/serialize.test.ts
+++ test/ut/serialize.test.ts
@@ -34,4 +34,28 @@ describe('Serialization and Deserialization', () => {
     }
   });
 
+  it("Make sure that all important classes are serializable", function () {
+
+    function testSerializeDeserialize(message: common.Message) {
+      var jsonText: string = JSON.stringify(message);
+      var resultMessage: common.Message = common.SerializationUtils.deserialize(JSON.parse(jsonText), common);
+      var jsonTextAfter: string = JSON.stringify(resultMessage);
+      assert.equal(jsonText, jsonTextAfter);
+    }
+
+    testSerializeDeserialize(new common.Element());
+    testSerializeDeserialize(new common.Dimension2D(1, 2));
+    testSerializeDeserialize(new common.Dimension3D(1, 2, 3));
+    testSerializeDeserialize(new common.Distance(100.22));
+    testSerializeDeserialize(new common.Line2D(new common.Point2D(1, 2), new common.Point2D(3, 4)));
+    testSerializeDeserialize(new common.Line3D(new common.Point3D(1, 2, 3), new common.Point3D(4, 5, 6)));
+
+    testSerializeDeserialize(new common.Message());
+    testSerializeDeserialize(new common.GyroscopeAutoIntervalCommand(123));
+    testSerializeDeserialize(new common.GyroscopeValueResponse(common.AngleUtils.createAngleFromDegrees(7)));
+    testSerializeDeserialize(new common.OrientationRequest(common.AngleUtils.createAngleFromDegrees(55), common.AngleUtils.createAngleFromDegrees(10), 123));
+    testSerializeDeserialize(new common.OrientationResponse(true, common.AngleUtils.createAngleFromDegrees(78)));
+
+  });
+
 });
